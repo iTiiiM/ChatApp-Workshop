20898f8281532e7c5336690abbe310b76f4a54c7
Piyatat Thianboonsong <mewloft@gmail.com>
2020-02-13 14:41:52 +0700

    Initial Commit

---
+++ Messaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messaging
+//
+//  Created by Piyatat  Thianboonsong on 13/2/2563 BE.
+//  Copyright © 2563 iTiiiM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
