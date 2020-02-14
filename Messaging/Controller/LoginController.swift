bf5f6d28a7df60fc5c889c38c80f400c38e60f1f
prapawity <protfc16@gmail.com>
2020-02-14 15:24:04 +0700

    ✅ decorate UI

--- Messaging/Controller/LoginController.swift
+++ Messaging/Controller/LoginController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginButton.layer.cornerRadius = 10
         db = Firestore.firestore()
     }
     
