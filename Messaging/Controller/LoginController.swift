0d1ccf487625174b5af37d7818b089e66389f2ea
Piyatat Thianboonsong <mewloft@gmail.com>
2020-02-14 13:38:08 +0700

    try displayname

--- Messaging/Controller/LoginController.swift
+++ Messaging/Controller/LoginController.swift
@@ -13,14 +13,17 @@ import Firebase
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var displayNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var loginButton: UIButton!
     var db: Firestore?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.layer.cornerRadius = 10
         db = Firestore.firestore()
     }
     
@@ -30,7 +33,13 @@ class ViewController: UIViewController {
             if error != nil {
                 print(error!)
             } else {
+               
                 self.db?.collection("users").addDocument(data: ["email": self.emailTextField.text ?? ""])
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = self.displayNameTextField.text
+                }
                 let alert = UIAlertController(title: "Register success", message: "", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default))
                 self.present(alert, animated: true, completion: nil)
